d5e1b8c3e141ae3b920bfee1fa39a8a48256f212
SalgadoSteffania <stefani28sg@gmail.com>
2022-04-26 16:03:40 -0600

    Add RafContext

---
+++ WeatherConcurrencyApp.Infrastructure/Repository/RafContex.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace WeatherConcurrencyApp.Infrastructure.Repository
+{
+    public class RafContex
+   {
+
+    private string fileName;
+    private int size;
+
+    public RafContex(string fileName, int size)
+    {
+        this.fileName = fileName;
+        this.size = size;
+    }
+
+    public Stream HeaderStream
+    {
+        get => File.Open($"{fileName}.hd", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+    }
+
+    public Stream DataStream
+    {
+        get => File.Open($"{fileName}.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+    }
+
+
+        ///////////////////////////////////
+
+        public T Get<T>(int id)
+        {
+            try
+            {
+                T newValue = (T)Activator.CreateInstance(typeof(T));
+                int n = 0, k = 0;
+                using (BinaryReader brHeader = new BinaryReader(HeaderStream),
+                                    brData = new BinaryReader(DataStream))
+                {
+                    if (brHeader.BaseStream.Length > 0)
+                    {
+                        brHeader.BaseStream.Seek(0, SeekOrigin.Begin);
+                        n = brHeader.ReadInt32();
+                        k = brHeader.ReadInt32();
+                    }
+
+                    if (n == 0)
+                    {
+                        return default(T);
+                    }
+
+                    if (id <= 0 || id > k)
+                    {
+                        return default(T);
+                    }
+
+                    PropertyInfo[] properties = newValue.GetType().GetProperties();
+                    long posh = 8 + (id - 1) * 4;
+                    //TODO Add Binary search to find the id
+                    brHeader.BaseStream.Seek(posh, SeekOrigin.Begin);
+                    int index = brHeader.ReadInt32();
+
+                    if (index != id)
+                    {
+                        return default(T);
+                    }
+
+                    //long posd = (index - 1) * size;
+                    //brData.BaseStream.Seek(posd, SeekOrigin.Begin);
+                    //foreach (PropertyInfo pinfo in properties)
+                    //{
+                    //    Type type = pinfo.PropertyType;
+
+                    //    if (type.IsGenericType)
+                    //    {
+                    //        continue;
+                    //    }
+
+                    //    if (type == typeof(int))
+                    //    {
+                    //        pinfo.SetValue(newValue, brData.GetValue<int>(TypeCode.Int32));
+                    //    }
+                    //    else if (type == typeof(long))
+                    //    {
+                    //        pinfo.SetValue(newValue, brData.GetValue<long>(TypeCode.Int64));
+                    //    }
+                    //    else if (type == typeof(float))
+                    //    {
+                    //        pinfo.SetValue(newValue, brData.GetValue<float>(TypeCode.Single));
+                    //    }
+                    //    else if (type == typeof(double))
+                    //    {
+                    //        pinfo.SetValue(newValue, brData.GetValue<double>(TypeCode.Double));
+                    //    }
+                    //    else if (type == typeof(decimal))
+                    //    {
+                    //        pinfo.SetValue(newValue, brData.GetValue<decimal>(TypeCode.Decimal));
+                    //    }
+                    //    else if (type == typeof(char))
+                    //    {
+                    //        pinfo.SetValue(newValue, brData.GetValue<char>(TypeCode.Char));
+                    //    }
+                    //    else if (type == typeof(bool))
+                    //    {
+                    //        pinfo.SetValue(newValue, brData.GetValue<bool>(TypeCode.Boolean));
+                    //    }
+                    //    else if (type == typeof(string))
+                    //    {
+                    //        pinfo.SetValue(newValue, brData.GetValue<string>(TypeCode.String));
+                    //    }
+                    //}
+                }
+                return newValue;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+        }
+
+
+        public List<T> GetAll<T>()
+        {
+            List<T> listT = new List<T>();
+            int n = 0;
+            try
+            {
+                using (BinaryReader brHeader = new BinaryReader(HeaderStream))
+                {
+                    if (brHeader.BaseStream.Length > 0)
+                    {
+                        brHeader.BaseStream.Seek(0, SeekOrigin.Begin);
+                        n = brHeader.ReadInt32();
+                    }
+                }
+
+                if (n == 0)
+                {
+                    return listT;
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    int index;
+                    using (BinaryReader brHeader = new BinaryReader(HeaderStream))
+                    {
+                        long posh = 8 + i * 4;
+                        brHeader.BaseStream.Seek(posh, SeekOrigin.Begin);
+                        index = brHeader.ReadInt32();
+                    }
+
+                    T t = Get<T>(index);
+                    listT.Add(t);
+                }
+
+                return listT;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+
+
+        public int GetLastId()
+        {
+            int k = 0;
+
+            try
+            {
+                using (BinaryReader brHeader = new BinaryReader(HeaderStream))
+                {
+                    if (brHeader.BaseStream.Length <= 0)
+                    {
+                        return k;
+                    }
+
+                    long posh = 4;
+                    brHeader.BaseStream.Seek(posh, SeekOrigin.Begin);
+                    k = brHeader.ReadInt32();
+                    return k;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }////
+
+}///
+
+
+
+
+
+
