76fffc54e9393248904463ae48eda11d390d8640
Steffania Salgado <86125323+SalgadoSteffania@users.noreply.github.com>
2022-05-02 15:45:57 -0600

    Add files via upload

---
+++ WeatherConcurrencyApp.Infrastructure/Repository/JsonCities.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using WeatherConcurrentApp.Domain.Entities;
+using WeatherConcurrentApp.Domain.Interfaces;
+
+namespace WeatherConcurrencyApp.Infrastructure.Repository
+{
+    public class JsonCities : IJsonModel
+    {
+        public List<string> DeserializeJsonFile(string citiesJsonFromFile)
+        {
+            List<string> Cties = new List<string>();
+            List<Ciudad> Ciudades = JsonConvert.DeserializeObject<List<Ciudad>>(citiesJsonFromFile);
+            foreach (Ciudad item in Ciudades)
+            {
+                Cties.Add(item.city);
+            }
+            return Cties;
+        }
+
+        public string GetCitiesJsonFromFile(string path)
+        {
+            string CitysJsonfromfile;
+            using (var reader = new StreamReader(path))
+            {
+                CitysJsonfromfile = reader.ReadToEnd();
+            }
+            return CitysJsonfromfile;
+        }
+    }
+}
