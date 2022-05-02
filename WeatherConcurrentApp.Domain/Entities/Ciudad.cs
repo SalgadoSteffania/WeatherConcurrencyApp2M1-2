e91b4288431e2d953eff2c8cc06fec8b0689cfd8
Steffania Salgado <86125323+SalgadoSteffania@users.noreply.github.com>
2022-05-02 16:02:07 -0600

    Add files via upload

---
+++ WeatherConcurrentApp.Domain/Entities/Ciudad.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherConcurrentApp.Domain.Entities
+{
+    public class Ciudad
+    {
+
+        public string city { get; set; }
+    }
+}
