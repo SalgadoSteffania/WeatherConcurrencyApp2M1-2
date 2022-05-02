6c8f449ae861a388d46cebba00724724819ecdf6
Steffania Salgado <86125323+SalgadoSteffania@users.noreply.github.com>
2022-05-02 16:02:38 -0600

    Add files via upload

---
+++ WeatherConcurrentApp.Domain/Interfaces/IJsonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherConcurrentApp.Domain.Interfaces
+{
+    public interface IJsonModel
+    {
+        string GetCitiesJsonFromFile(string path);
+
+        List<string> DeserializeJsonFile(string citiesJsonFromFile);
+    }
+}
